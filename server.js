18c1316ceda00ac5d4c2c55c37e1ffb5dcf22644
Darshan S <darshans2727@gmail.com>
2025-06-17 01:35:58 +0530

    Removed nodemon.json and tsconfig.json files, updated package.json to simplify scripts and dependencies, and upgraded several package versions in pnpm-lock.yaml.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 
@@ -11,22 +11,25 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
     res.json({
         message: {
-            "tmdb_api_key_available": process.env.TMDB_API_KEY !== undefined,
-            "tmdb_access_token_available": process.env.TMDB_ACCESS_TOKEN !== undefined
+            tmdb_api_key_available: process.env.TMDB_API_KEY !== undefined,
+            tmdb_access_token_available:
+                process.env.TMDB_ACCESS_TOKEN !== undefined,
         },
         params: req.params,
         query: req.query,
         body: req.body,
-        headers: req.headers
+        headers: req.headers,
     });
 });
 
-app.post("/fetch-from-tmdb", async (req: Request, res: Response) => {
-    const { url }: { url: string } = req.body;
-    const newUrl = url.includes("?") ? `${url}&api_key=${process.env.TMDB_API_KEY}` : `${url}?api_key=${process.env.TMDB_API_KEY}`;
+app.post("/fetch-from-tmdb", async (req, res) => {
+    const { url } = req.body;
+    const newUrl = url.includes("?")
+        ? `${url}&api_key=${process.env.TMDB_API_KEY}`
+        : `${url}?api_key=${process.env.TMDB_API_KEY}`;
     try {
         const response = await fetch(newUrl);
         const resJson = await response.json();
